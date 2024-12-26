8816803460d472285bc50162ebf62cd5c5e85253
Vikyath Shirva <vicky.fames@gmail.com>
2024-12-26 12:17:23 +0530

    added basic excetions

---
+++ api/src/main/java/com/vikyathshirva/api/core/exceptions/InvalidInputException.java
@@ -0,0 +1,22 @@
+package com.vikyathshirva.api.core.exceptions;
+
+
+/**
+ * InvalidInputException
+ */
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException(){};
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidInputException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ api/src/main/java/com/vikyathshirva/api/core/exceptions/NotFoundException.java
@@ -0,0 +1,22 @@
+package com.vikyathshirva.api.core.exceptions;
+
+/**
+ * NotFoundException
+ */
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(){}
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause){
+        super(cause);
+    }
+
+}
